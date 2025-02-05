5c83f748656fa5e4ec763518d7465bd0131be997
Meteor <coopticks@outlook.com>
2025-02-05 10:03:38 +0000

    test change

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
-
+import * as github from '@actions/github'
 /**
  * The main function for the action.
  *
@@ -8,18 +7,10 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Hello ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const sha: string = core.getInput('sha')
+    const token = core.getInput('token')
+    console.log(github.getOctokit(token))
+    core.debug(sha)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
